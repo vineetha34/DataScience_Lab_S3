edf4977aefc0afd5401c9d4dd4fbb7cbc00bd081
vineetha34 <75126810+vineetha34@users.noreply.github.com>
2022-02-28 21:17:07 +0530

    Update Exercise_7.py

--- Exercise_7.py
+++ Exercise_7.py
@@ -17,3 +17,28 @@ y_pred = classifier.predict(x_test)
 from sklearn.metrics import accuracy_score
 print('Accuracy on train data using Gini: ',accuracy_score(y_true = y_train,y_pred = classifier.predict(x_train)))
 print('Accuracy on test data using Gini: ',accuracy_score(y_true = y_test,y_pred = y_pred))
+#change criterion to entropy
+classifier_entropy = DecisionTreeClassifier(criterion='entropy')
+classifier_entropy.fit(X_train, y_train)
+y_pred_entropy = classifier_entropy.predict(X_test)
+print('Accuracy on train data using entropy', accuracy_score(y_true=y_train, y_pred = classifier_entropy.predict(X_train)))
+print('Accuracy on test data using entropy', accuracy_score(y_true=y_test, y_pred = y_pred_entropy))
+#change criterion to entropy with min_samples_split to 50. Default value is 2
+classifier_entropy1 = DecisionTreeClassifier(criterion='entropy', min_samples_split=50)
+classifier_entropy1.fit(X_train, y_train)
+y_pred_entropy1 = classifier_entropy1.predict(X_test)
+print('Accuracy on train data using entropy', accuracy_score(y_true=y_train, y_pred = classifier_entropy1.predict(X_train)))
+print('Accuracy on test data using entropy', accuracy_score(y_true=y_test, y_pred = y_pred_entropy1))
+#visualise the decision tree
+
+from sklearn.tree import export_graphviz
+from six import StringIO
+from IPython.display import Image
+import pydotplus
+
+dot_data = StringIO()
+#the students can try using classifier, classifier_entropy and classifier_entropy1
+#as first parameter below.
+export_graphviz(classifier, out_file = dot_data,filled = True, rounded = True,special_characters = True, feature_names = data.feature_names, class_names = data.target_names)
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+Image(graph.create_png())
